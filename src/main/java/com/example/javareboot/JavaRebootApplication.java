52972bb640caefc8e71a1ffe3ef06a852e354a95
hupeng <hupeng1@xiaomi.com>
2019-09-27 17:35:16 +0800

    11

--- src/main/java/com/example/javareboot/JavaRebootApplication.java
+++ src/main/java/com/example/javareboot/JavaRebootApplication.java
@@ -15,8 +15,8 @@ public class JavaRebootApplication {
 	public void run(String... args) throws Exception {
 		Robot robot = new Robot();
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		list.add(87);
-		list.add(83);
+//		list.add(87);
+//		list.add(83);
 		list.add(65);
 		list.add(68);
 		list.add(69);
@@ -25,14 +25,14 @@ public class JavaRebootApplication {
 		list.add(84);
 		list.add(90);
 		while (true){
-			Thread.sleep(3*1000);
+			Thread.sleep(60*1000);
 			int max=list.size()-1;
 			Random generate = new Random();
 			int j = generate.nextInt(max);
 			int key=list.get(j);
-			for(int i=0;i<10;i++){
+			for(int i=0;i<5;i++){
 				robot.keyPress(key);
-				robot.delay(2000);
+				robot.delay(1000);
 				robot.keyRelease(key);
 			}
 		}
