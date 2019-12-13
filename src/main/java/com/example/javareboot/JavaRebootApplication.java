52b0c5475ffb311e0fe96092341eb3954fc9fcc4
hupeng <hupeng1@xiaomi.com>
2019-12-13 17:54:43 +0800

    11

--- src/main/java/com/example/javareboot/JavaRebootApplication.java
+++ src/main/java/com/example/javareboot/JavaRebootApplication.java
@@ -6,8 +6,18 @@ import java.util.Random;
 
 public class JavaRebootApplication {
 
+	private static final Integer W=87;
+	private static final Integer S=83;
+	private static final Integer A=65;
+	private static final Integer D=68;
+	private static final Integer X=88;
+	private static final Integer J=74;
+	private static final Integer Q=81;
+
+
+
 	public static void main(String[] args) throws Exception {
-		System.out.println("防掉线服务已经启动。。。。。");
+		System.out.println("boot has start .............");
 		JavaRebootApplication j = new JavaRebootApplication();
 		j.run(null);
 	}
@@ -15,26 +25,33 @@ public class JavaRebootApplication {
 	public void run(String... args) throws Exception {
 		Robot robot = new Robot();
 		ArrayList<Integer> list = new ArrayList<Integer>();
-//		list.add(87);
-//		list.add(83);
-		list.add(65);
-		list.add(68);
-		list.add(69);
-		list.add(81);
-		list.add(82);
-		list.add(84);
-		list.add(90);
+		list.add(W);
+		list.add(S);
+		list.add(A);
+		list.add(D);
+		list.add(X);
+
+		//先设置为走动
+		robot.keyPress(J);
+		robot.delay(100);
+		robot.keyRelease(J);
+
+		//进入潜行
+		Thread.sleep(5*1000);
+		robot.keyPress(Q);
+		robot.delay(100);
+		robot.keyRelease(Q);
+
+
 		while (true){
-			Thread.sleep(60*1000);
+			Thread.sleep(30*1000);
 			int max=list.size()-1;
 			Random generate = new Random();
 			int j = generate.nextInt(max);
 			int key=list.get(j);
-			for(int i=0;i<5;i++){
-				robot.keyPress(key);
-				robot.delay(1000);
-				robot.keyRelease(key);
-			}
+			robot.keyPress(key);
+			robot.delay(2000);
+			robot.keyRelease(key);
 		}
 	}
 
